036327044aa739e386d04b78a1e47ec944ecefc3
Vladimir Korkin <korkin.dev@gmail.com>
2023-05-16 16:14:06 +0200

    add func getResource to get response from swapi

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log("Hello World");
+// https://swapi.dev/api/people/1/
+
+const getResource = async (url) => {
+  const response = await fetch(url);
+  const body = await response.json();
+  return body;
+};
+
+getResource("https://swapi.dev/api/people/1/").then((body) => {
+  console.log(body);
+});
+
+// fetch("https://swapi.dev/api/people/1/")
+//   .then((response) => {
+//     return response.json(); // return json from server
+//   })
+//   .then((bodyResponse) => {
+//     console.log(bodyResponse); // return body response
+//   });
