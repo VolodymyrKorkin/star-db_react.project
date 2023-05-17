7208d9c95415b68ebb7f56331fdb2d24f274806c
Vladimir Korkin <korkin.dev@gmail.com>
2023-05-17 16:31:53 +0200

    add error handling to func getResource

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
+/* eslint-disable no-useless-concat */
 // https://swapi.dev/api/people/1/
 
 const getResource = async (url) => {
   const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error(`Could not fetch ${url}` + `, received ${response.status}`);
+  }
+
   const body = await response.json();
   return body;
 };
 
-getResource("https://swapi.dev/api/people/1/").then((body) => {
-  console.log(body);
-});
+getResource("https://swapi.dev/api/people/123123/")
+  .then((body) => {
+    console.log(body);
+  })
+  .catch((err) => {
+    console.error("Could not fetch", err);
+  });
 
 // fetch("https://swapi.dev/api/people/1/")
 //   .then((response) => {
