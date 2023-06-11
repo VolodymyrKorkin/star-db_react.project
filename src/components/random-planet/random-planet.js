c69a78144d2d23701a0f17434e082001f162fec9
Vladimir Korkin <korkin.dev@gmail.com>
2023-06-11 12:51:02 +0200

    add random show planets, set time of update random planet

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -18,6 +18,9 @@ export default class RandomPlanet extends Component {
   constructor() {
     super();
     this.updatePlanet();
+    // taking id of this setInterval
+    this.interval = setInterval(this.updatePlanet, 1500);
+    // clearInterval(this.interval);
   }
 
   onPlanetLoaded = (planet) => {
@@ -34,13 +37,13 @@ export default class RandomPlanet extends Component {
     });
   };
 
-  updatePlanet() {
-    const id = 15000;
+  updatePlanet = () => {
+    const id = Math.floor(Math.random() * 25) + 3;
     this.swapiService
       .getPlanet(id)
       .then(this.onPlanetLoaded)
       .catch(this.onError);
-  }
+  };
 
   render() {
     const { planet, loading, error } = this.state;
